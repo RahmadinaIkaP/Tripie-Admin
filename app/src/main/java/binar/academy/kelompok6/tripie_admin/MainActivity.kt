7701e0a510175dbad9189b8d2a2e9cb86558dcf1
RahmadinaIkaP <rahmadina.ika@student.umn.ac.id>
2022-11-22 20:36:35 +0700

    Initial commit

---
+++ app/src/main/java/binar/academy/kelompok6/tripie_admin/MainActivity.kt
@@ -0,0 +1,11 @@
+package binar.academy.kelompok6.tripie_admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
